2f20f5d5468b45b11cf9ce6df80347c3345a4e38
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-04-01 18:47:29 -0400

    moved UIImageEffects to MenuHelper

--- Pod/Classes/JVMenuHelper.h
+++ Pod/Classes/JVMenuHelper.h
@@ -34,4 +34,32 @@
 
 + (UIVisualEffectView * )applyBlurrWithEfftectStyle:(UIBlurEffectStyle)style withFrame:(CGRect)frame;
 
+#pragma mark - UIImageEffects
++ (UIImage*)imageByApplyingLightEffectToImage:(UIImage*)inputImage;
++ (UIImage*)imageByApplyingExtraLightEffectToImage:(UIImage*)inputImage;
++ (UIImage*)imageByApplyingDarkEffectToImage:(UIImage*)inputImage;
++ (UIImage*)imageByApplyingTintEffectWithColor:(UIColor *)tintColor toImage:(UIImage*)inputImage;
+
+//| ----------------------------------------------------------------------------
+//! Applies a blur, tint color, and saturation adjustment to @a inputImage,
+//! optionally within the area specified by @a maskImage.
+//!
+//! @param  inputImage
+//!         The source image.  A modified copy of this image will be returned.
+//! @param  blurRadius
+//!         The radius of the blur in points.
+//! @param  tintColor
+//!         An optional UIColor object that is uniformly blended with the
+//!         result of the blur and saturation operations.  The alpha channel
+//!         of this color determines how strong the tint is.
+//! @param  saturationDeltaFactor
+//!         A value of 1.0 produces no change in the resulting image.  Values
+//!         less than 1.0 will desaturation the resulting image while values
+//!         greater than 1.0 will have the opposite effect.
+//! @param  maskImage
+//!         If specified, @a inputImage is only modified in the area(s) defined
+//!         by this mask.  This must be an image mask or it must meet the
+//!         requirements of the mask parameter of CGContextClipToMask.
++ (UIImage*)imageByApplyingBlurToImage:(UIImage*)inputImage withRadius:(CGFloat)blurRadius tintColor:(UIColor *)tintColor saturationDeltaFactor:(CGFloat)saturationDeltaFactor maskImage:(UIImage *)maskImage;
+
 @end

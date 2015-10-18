5518c08df2428fd88da66072258ad7823f549aa5
Jorge Valbuena <jorgevalbuena2@gmail.com>
2015-10-18 18:28:33 -0400

    started adding comments to the popover view controller.

--- Source/Classes/JVMenuControllers/JVMenuPopoverViewController.h
+++ Source/Classes/JVMenuControllers/JVMenuPopoverViewController.h
@@ -9,37 +9,100 @@
 #import <UIKit/UIKit.h>
 #import <JVMenuPopoverView.h>
 
+
 @class JVMenuPopoverViewController;
 
 #pragma mark - Protocol
+
 @protocol JVMenuPopoverViewControllerDelegate <NSObject>
 
 @required
+
+/**
+ Sets new view controller depending on selected @a indexPath.
+ 
+ @param navController
+    The navigation controller.
+ @param indexPath
+  The selected index path.
+ */
 - (void)setNewViewController:(UINavigationController *)navController fromIndexPath:(NSIndexPath *)indexPath;
 
+
 @optional
-- (void)showMenu:(JVMenuPopoverViewController *)JVMenuPopoverViewController inViewController:(UIViewController *)viewController;
-- (void)closeMenu:(JVMenuPopoverViewController *)JVMenuPopoverViewController;
+
+/**
+ Tells the navigation controller to present new view controller.
+ 
+ @param jvMenuPopoverViewController
+    The jv menu view controller.
+ @param viewController
+    The view controller where the menu will be shown.
+ */
+- (void)showMenu:(JVMenuPopoverViewController *)jvMenuPopoverViewController inViewController:(UIViewController *)viewController;
+
+
+/**
+ Tells the navigation controller to dismiss view controller.
+ 
+ @param jvMenuPopoverViewController
+    The jv menu view controller.
+ */
+- (void)closeMenu:(JVMenuPopoverViewController *)jvMenuPopoverViewController;
 
 @end
 
+
 #pragma mark - Interface
+
 @interface JVMenuPopoverViewController : UIViewController <JVMenuPopoverDelegate>
 
-// properties
 @property (nonatomic, weak) id <JVMenuPopoverViewControllerDelegate> delegate;
-@property (nonatomic, strong) JVMenuPopoverView *menuView;
-@property (nonatomic, strong) NSArray *images;
-@property (nonatomic, strong) NSArray *titles;
-@property (nonatomic, strong) UIImage *closeImage;
-@property (nonatomic, assign) BOOL slideInAnimation;
-@property (nonatomic, assign) BOOL slideInWithBounceAnimation;
 
-// initializer
+@property (nonatomic, strong, readwrite) JVMenuPopoverView *menuView;
+
+@property (nonatomic, strong, readwrite) NSArray *images;
+
+@property (nonatomic, strong, readwrite) NSArray *titles;
+
+@property (nonatomic, strong, readwrite) UIImage *closeImage;
+
+@property (nonatomic, assign, readwrite) BOOL slideInAnimation;
+
+@property (nonatomic, assign, readwrite) BOOL slideInWithBounceAnimation;
+
+
+/**
+ Custom initializer with an array of images, an array of titles and the close image.
+ 
+ @param images
+    An array holding the set of images to be used for each menu item.
+ @param titles
+    An array holding the set of titles to be used for each menu item.
+ @param closeImage
+    The image to be used for the close button.
+ 
+ @return
+    An instance of JVMenuPopoverViewController.
+ */
 - (instancetype)initWithImages:(NSArray *)images titles:(NSArray *)titles closeImage:(UIImage *)closeImage;
 
-// Menu helper functions
+
+/**
+ Shows @a JVMenuPopover from specified view controller.
+ 
+ @param viewController
+    The view controller where to show the menu from.
+ */
 - (void)showMenuFromController:(UIViewController *)viewController;
+
+
+/**
+ Closes @a JVMenuPopover from specified view controller.
+ 
+ @param viewController
+    The view controller where to close the menu from.
+ */
 - (void)closeMenuFromController:(UIViewController *)viewController;
 
 @end
